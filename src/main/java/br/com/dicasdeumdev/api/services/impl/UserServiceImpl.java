aad159fbf95b2ca2cdfab31104bbb9dfb945662f
Valdir Cezar <valdircezar312@gmail.com>
2021-09-24 16:15:01 -0300

    classe user service impl findById

---
+++ src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.dicasdeumdev.api.services.impl;
+
+import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.repositories.UserRepository;
+import br.com.dicasdeumdev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
