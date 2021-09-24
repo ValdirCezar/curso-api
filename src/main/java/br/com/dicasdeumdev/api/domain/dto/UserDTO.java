a73b960e5c226dafcf2d3f6bf6f67bc80de866f3
Valdir Cezar <valdircezar312@gmail.com>
2021-09-24 19:52:27 -0300

    classe user dto

---
+++ src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package br.com.dicasdeumdev.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
