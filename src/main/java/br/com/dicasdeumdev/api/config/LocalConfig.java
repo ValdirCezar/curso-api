e8f45083db4655b427b02d9136e4e3a911304d7f
Valdir Cezar <valdircezar312@gmail.com>
2021-09-24 16:38:31 -0300

    perfil de testes

---
+++ src/main/java/br/com/dicasdeumdev/api/config/LocalConfig.java
@@ -0,0 +1,26 @@
+package br.com.dicasdeumdev.api.config;
+
+import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Bean
+    public void startDB() {
+        User u1 = new User(null, "Valdir", "valdir@mail.com", "123");
+        User u2 = new User(null, "Luiz", "luiz@mail.com", "123");
+
+        repository.saveAll(List.of(u1, u2));
+    }
+}
