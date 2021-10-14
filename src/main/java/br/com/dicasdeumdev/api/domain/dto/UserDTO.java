981e9b3f537de3e6d62a1671c2a690c9c63f17e5
Valdir Cezar <valdircezar312@gmail.com>
2021-10-14 16:51:35 -0300

    removendo anotacoes nao utilizadas

--- src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
@@ -3,12 +3,10 @@ package br.com.dicasdeumdev.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
--- src/main/java/br/com/dicasdeumdev/api/resources/exceptions/StandardError.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/exceptions/StandardError.java
@@ -2,12 +2,10 @@ package br.com.dicasdeumdev.api.resources.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandardError {
 
